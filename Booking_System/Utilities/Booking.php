a3baac024166e4dc988a6b3f2b1a319d6bf66625
Ahmad Wael <dev.ahmedwael@gmail.com>
2020-09-17 16:52:30 +0200

    building booking class

--- Booking_System/Utilities/Booking.php
+++ Booking_System/Utilities/Booking.php
@@ -3,11 +3,47 @@
 namespace Booking_System\Utilities;
 
 class Booking {
-
 	protected $bookings_storage = 'wcb-bookings';
+
+	private $order_id;
+	private $booking_product_id;
+	private $booking_date;
+	private $booking_time;
+	private $persons_count;
 
 	public function __construct( $booking = '' ) {
 
+	}
+
+	public function set_order_id( $order_id ) {
+		$this->order_id = $order_id;
+	}
+
+	public function set_booking_product_id( $booking_product_id ) {
+		$this->booking_product_id = $booking_product_id;
+	}
+
+	public function set_booking_date( $booking_date ) {
+		$this->booking_date = $booking_date;
+	}
+
+	public function set_booking_time( $booking_time ) {
+		$this->booking_time = $booking_time;
+	}
+
+	public function set_persons_count( $persons_count ) {
+		$this->persons_count = $persons_count;
+	}
+
+	function save() {
+		$booking_data = array(
+			'post_title'  => $this->booking_date . ' :: ' . $this->booking_time,
+			'post_status' => 'publish',
+			'post_type'   => $this->bookings_storage,
+			'meta_input'  => array(),
+		);
+
+		return wp_insert_post( $booking_data, true );
 	}
 
 }
