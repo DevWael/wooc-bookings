2b74c2b2c8fd263c49eca25a69e2c89cd316b2af
Ahmad Wael <dev.ahmedwael@gmail.com>
2020-09-17 14:37:13 +0200

    display booking data on order

--- Booking_System/WC/Order.php
+++ Booking_System/WC/Order.php
@@ -4,4 +4,44 @@ namespace Booking_System\WC;
 
 class Order {
 
+	public function order_data( $item, $cart_item_key, $values, $order ) {
+		if ( isset( $values['booking_date'] ) ) {
+			$item->add_meta_data(
+				__( 'Booking Date', 'wcb' ),
+				$values['booking_date'],
+				true
+			);
+		}
+
+		if ( isset( $values['booking_time'] ) ) {
+			$item->add_meta_data(
+				__( 'Booking Time', 'wcb' ),
+				$values['booking_time'],
+				true
+			);
+		}
+
+		if ( isset( $values['persons_count'] ) ) {
+			$item->add_meta_data(
+				__( 'Persons Count', 'wcb' ),
+				$values['persons_count'],
+				true
+			);
+		}
+
+		if ( isset( $values['person_price'] ) ) {
+			$max_persons   = $values['max_persons_count'];
+			$persons_count = $values['persons_count'];
+			$person_price  = $values['person_price'];
+			if ( $persons_count > $max_persons ) {
+				$extra_persons = $persons_count - $max_persons;
+				$extra_price   = $person_price * $extra_persons;
+				$item->add_meta_data(
+					__( 'Extra Persons Price', 'wcb' ),
+					$extra_price . ' ' . get_woocommerce_currency_symbol(),
+					true
+				);
+			}
+		}
+	}
 }
--- includes/class-wcb.php
+++ includes/class-wcb.php
@@ -86,6 +86,7 @@ class Wcb {
 		$this->bookings();
 		$this->product_booking_form();
 		$this->cart_process();
+		$this->order_data();
 
 	}
 
@@ -185,6 +186,12 @@ class Wcb {
 		$this->loader->add_filter( 'woocommerce_add_to_cart_validation', $cart, 'validate_cart_data', 10, 4 );
 		$this->loader->add_filter( 'woocommerce_before_calculate_totals', $cart, 'calculate_cart_total', 25, 3  );
 		$this->loader->add_filter( 'woocommerce_get_item_data', $cart, 'display_cart_data', 10, 2 );
+	}
+
+	private function order_data() {
+		$order_data = new \Booking_System\WC\Order();
+		$this->loader->add_action( 'woocommerce_checkout_create_order_line_item', $order_data, 'order_data', 10, 4 );
+//		$this->loader->add_filter( 'woocommerce_order_item_name', $order_data, 'order_email_data', 10, 2 );
 	}
 
 	/**
